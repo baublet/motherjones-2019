7906b6c572c1590f4a2701a148507177c4d3748b
Ryan Poe <baublet@gmail.com>
2019-01-11 13:18:31 -0600

    Update woocom

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -1,5 +1,4 @@
 <?php
-
 global $product;
 
 // Ensure visibility.
@@ -8,8 +7,7 @@ if (empty($product) || ! $product->is_visible()) {
 }
 ?>
 <li <?php wc_product_class('w-1/3'); ?>>
-	<?php
-
+    <?php
     do_action('woocommerce_before_shop_loop_item');
     do_action('woocommerce_before_shop_loop_item_title');
     do_action('woocommerce_shop_loop_item_title');
