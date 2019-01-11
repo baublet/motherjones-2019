a84747833f71e277131f2b52038496309d546382
Ryan Poe <baublet@gmail.com>
2019-01-11 13:22:45 -0600

    Update woocom

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -8,10 +8,10 @@ if (empty($product) || ! $product->is_visible()) {
 ?>
 <li <?php wc_product_class('w-1/3'); ?>>
     <?php
-    do_action('woocommerce_before_shop_loop_item');
-    do_action('woocommerce_before_shop_loop_item_title');
-    do_action('woocommerce_shop_loop_item_title');
-    do_action('woocommerce_after_shop_loop_item_title');
-    do_action('woocommerce_after_shop_loop_item');
+    do_action('woocommerce_before_shop_loop_item'); echo 'a';
+    do_action('woocommerce_before_shop_loop_item_title'); echo 'b';
+    do_action('woocommerce_shop_loop_item_title'); echo 'c';
+    do_action('woocommerce_after_shop_loop_item_title'); echo 'd';
+    do_action('woocommerce_after_shop_loop_item'); echo 'e';
     ?>
 </li>
