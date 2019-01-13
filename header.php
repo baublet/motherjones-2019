51eae118e9c98e7f701f86eb67f0a15fb3372e07
Ryan Poe <baublet@gmail.com>
2019-01-13 12:19:39 -0600

    Update template

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
             </div>
             <div class="navigation">
                 <input type="checkbox" id="headerNav" class="toggleCheck hidden" />
-                <label for="headerNav" class="cursor-pointer p-sm block desktop:hidden bg-primary text-primaryText toggleChecked:bg-foreground toggleChecked:text-background flex justify-between items-center hover:bg-primaryDark">
+                <label for="headerNav" class="cursor-pointer p-sm block desktop:hidden bg-primaryDark text-primaryText toggleChecked:bg-foreground toggleChecked:text-background flex justify-between items-center hover:bg-foreground">
                     <span>Navigation</span>
                     <i class="fas fa-ellipsis-v"></i>
                 </label>
